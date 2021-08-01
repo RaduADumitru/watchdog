f9a450af70943841de81828f2ea7b3451b8fc231
RaduADumitru <radu.dumitru@rocketmail.com>
2021-08-01 19:15:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,24 @@ import psutil
 import yaml
 
 
+resources = {'CORE': resource.RLIMIT_CORE,
+             'CPU': resource.RLIMIT_CPU,
+             'FSIZE': resource.RLIMIT_FSIZE,
+             'DATA': resource.RLIMIT_DATA,
+             'STACK': resource.RLIMIT_STACK,
+             'RSS': resource.RLIMIT_RSS,
+             'NPROC': resource.RLIMIT_NPROC,
+             'NOFILE': resource.RLIMIT_NOFILE,
+             'OFILE': resource.RLIMIT_OFILE,
+             'MEMLOCK': resource.RLIMIT_MEMLOCK,
+             'AS': resource.RLIMIT_AS,
+             'MSGQUEUE': resource.RLIMIT_MSGQUEUE,
+             'NICE': resource.RLIMIT_NICE,
+             'RTPRIO': resource.RLIMIT_RTPRIO,
+             'RTTIME': resource.RLIMIT_RTTIME,
+             'SIGPENDING': resource.RLIMIT_SIGPENDING
+             }
+
 f = open('config.yaml', 'r')
 procs = yaml.load(f, Loader=yaml.FullLoader)
 f.close()
@@ -17,87 +35,25 @@ for p in procs['limits'].keys():
             pid = proc.pid
             path = proc.exe()
             pidlist.append(pid)
-            break
-    if (path == ''):
+    if path == '':
         raise ('Process ' + name + ' not found\n')
     p1 = psutil.Process(pid)
     fields = procs['limits'][p].keys()
     for limres in fields:
-        value = procs['limits'][p][limres]
-        if limres == 'CORE':
-            resource.prlimit(pid, resource.RLIMIT_CORE, (value, value))
-        elif limres == 'CPU':
-            resource.prlimit(pid, resource.RLIMIT_CPU, (value, value))
-        elif limres == 'FSIZE':
-            resource.prlimit(pid, resource.RLIMIT_FSIZE, (value, value))
-        elif limres == 'DATA':
-            resource.prlimit(pid, resource.RLIMIT_DATA, (value, value))
-        elif limres == 'STACK':
-            resource.prlimit(pid, resource.RLIMIT_STACK, (value, value))
-        elif limres == 'RSS':
-            resource.prlimit(pid, resource.RLIMIT_RSS, (value, value))
-        elif limres == 'NPROC':
-            resource.prlimit(pid, resource.RLIMIT_NPROC, (value, value))
-        elif limres == 'NOFILE':
-            resource.prlimit(pid, resource.RLIMIT_NOFILE, (value, value))
-        elif limres == 'OFILE':
-            resource.prlimit(pid, resource.RLIMIT_OFILE, (value, value))
-        elif limres == 'MEMLOCK':
-            resource.prlimit(pid, resource.RLIMIT_MEMLOCK, (value, value))
-        elif limres == 'AS':
-            resource.prlimit(pid, resource.RLIMIT_AS, (value, value))
-        elif limres == 'MSGQUEUE':
-            resource.prlimit(pid, resource.RLIMIT_MSGQUEUE, (value, value))
-        elif limres == 'NICE':
-            resource.prlimit(pid, resource.RLIMIT_NICE, (value, value))
-        elif limres == 'RTPRIO':
-            resource.prlimit(pid, resource.RLIMIT_RTPRIO, (value, value))
-        elif limres == 'RTTIME':
-            resource.prlimit(pid, resource.RLIMIT_RTTIME, (value, value))
-        elif limres == 'SIGPENDING':
-            resource.prlimit(pid, resource.RLIMIT_SIGPENDING, (value, value))
-
+        if limres in resources.keys():
+            value = procs['limits'][p][limres]
+            resource.prlimit(pid, resources[limres], (value, value))
 while True:
     for pid in pidlist:
         p1 = psutil.Process(pid)
         path = p1.exe()
-        if not (p1.is_running()) or (p1.status() == psutil.STATUS_ZOMBIE):  # WIP
+        if not (p1.is_running()) or (p1.status() == psutil.STATUS_ZOMBIE):
             p1 = psutil.Popen(path)
             print('Process ' + p1.name() + ' filepath(' + p1.exe() + ') restarted\n')
             pid = p1.pid()
             fields = procs['limits'][p1.name()].keys()
             for limres in fields:
-                value = procs['limits'][p1.name()][limres]
-                if limres == 'CORE':
-                    resource.prlimit(pid, resource.RLIMIT_CORE, (value, value))
-                elif limres == 'CPU':
-                    resource.prlimit(pid, resource.RLIMIT_CPU, (value, value))
-                elif limres == 'FSIZE':
-                    resource.prlimit(pid, resource.RLIMIT_FSIZE, (value, value))
-                elif limres == 'DATA':
-                    resource.prlimit(pid, resource.RLIMIT_DATA, (value, value))
-                elif limres == 'STACK':
-                    resource.prlimit(pid, resource.RLIMIT_STACK, (value, value))
-                elif limres == 'RSS':
-                    resource.prlimit(pid, resource.RLIMIT_RSS, (value, value))
-                elif limres == 'NPROC':
-                    resource.prlimit(pid, resource.RLIMIT_NPROC, (value, value))
-                elif limres == 'NOFILE':
-                    resource.prlimit(pid, resource.RLIMIT_NOFILE, (value, value))
-                elif limres == 'OFILE':
-                    resource.prlimit(pid, resource.RLIMIT_OFILE, (value, value))
-                elif limres == 'MEMLOCK':
-                    resource.prlimit(pid, resource.RLIMIT_MEMLOCK, (value, value))
-                elif limres == 'AS':
-                    resource.prlimit(pid, resource.RLIMIT_AS, (value, value))
-                elif limres == 'MSGQUEUE':
-                    resource.prlimit(pid, resource.RLIMIT_MSGQUEUE, (value, value))
-                elif limres == 'NICE':
-                    resource.prlimit(pid, resource.RLIMIT_NICE, (value, value))
-                elif limres == 'RTPRIO':
-                    resource.prlimit(pid, resource.RLIMIT_RTPRIO, (value, value))
-                elif limres == 'RTTIME':
-                    resource.prlimit(pid, resource.RLIMIT_RTTIME, (value, value))
-                elif limres == 'SIGPENDING':
-                    resource.prlimit(pid, resource.RLIMIT_SIGPENDING, (value, value))
+                if limres in resources.keys():
+                    value = procs['limits'][p1.name()][limres]
+                    resource.prlimit(pid, resources[limres], (value, value))
     time.sleep(1)
